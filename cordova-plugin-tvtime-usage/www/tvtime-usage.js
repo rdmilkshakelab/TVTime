0e83a62e8aa9b74a1e6407732f3a4d2dd0584f88
rdmilkshakelab <alfiechateau@outlook.com>
2026-09-21 16:53:46 +0100

    Add TVTimeUsage methods for access and settings

--- cordova-plugin-tvtime-usage/www/tvtime-usage.js
+++ cordova-plugin-tvtime-usage/www/tvtime-usage.js
@@ -1,1 +1,15 @@
+var exec = require('cordova/exec');
 
+module.exports = {
+    hasAccess: function (success, failure) {
+        exec(success, failure, 'TVTimeUsage', 'hasAccess', []);
+    },
+
+    openSettings: function (success, failure) {
+        exec(success, failure, 'TVTimeUsage', 'openSettings', []);
+    },
+
+    queryToday: function (success, failure) {
+        exec(success, failure, 'TVTimeUsage', 'queryToday', []);
+    }
+};
